6f4b94077d3645df53e0b3feab0fee6c1c6ee320
Robert Fink <robert.fink@gsa.gov>
2025-01-19 18:55:20 -0500

    set all coverage back to 80 percent

--- javascriptapp/jest.config.js
+++ javascriptapp/jest.config.js
@@ -3,8 +3,7 @@ module.exports = {
   // ... other config ...
   coverageThreshold: {
     global: {
-      // branches relaxed since online Github Actions will not have a valid API key
-      branches: 60,
+      branches: 80,
       functions: 80,
       lines: 80,
       statements: 80
